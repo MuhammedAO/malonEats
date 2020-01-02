f5e33e59e68d45f813414dbcb93497667a7cb6a1
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-01-02 12:19:15 +0100

    display menu items

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -16,10 +16,25 @@ export default class Menu extends Component {
             <section className="menu py-5">
              <div className="container">
             <Title title="best of our menu"/>  
-            <div className="row mb-5">
-            <div className="col">
-               <h1>Our items</h1> 
-            </div>
+            <div className="row">
+             {this.state.maloncuisine.map(({node}) =>{
+              return(
+              <div key={node.id} className="col-11 col-md-6 my-3 d-flex mx-auto">
+                  <div>
+               <Img fixed={node.image.fixed}/>
+                  </div>
+                  <div className="flex-grow-1 px-3">
+                      <div className="d-flex justify-content-between">
+                          <h6 className="mb-0">{node.title}</h6>
+                          <h6 className="mb-0 brown-text">&#x20a6;{node.price}</h6>
+                      </div>
+                   <p className="text-muted">
+                    <small>{node.description.description}</small>
+                   </p>
+                  </div>
+              </div>
+              )
+             })}
             </div>
             </div>   
             </section>
